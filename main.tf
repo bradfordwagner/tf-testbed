6a6054846e9f540e81573df7f47db7dfe6918cbc
Bradford Wagner <bwagner@blackrock.com>
2021-12-07 13:50:02 -0500

    setup defaul override

--- main.tf
+++ main.tf
@@ -1,91 +1,36 @@
-variable "regions" {
-  default = ["del", "ewd", "hal"]
+variable "calc_overrides" {
+  description    = "release_link -> {variables}"
+  default        = {
+    alpha        = {}
+    beta         = {
+      chart      = "astra-calc-server"
+      is_enabled = false
+      version    = "1.2.3"
+    }
+  }
 }
 
-variable "files" {
-  default = ["ctx", "conf", "ca"]
-}
-
-variable "aladdin_client" {
-  default = "dev"
-}
-
-variable "mi6_labels" {
-  default = {
-    groups      = ["group1", "group2"]
-    owners      = ["owner1", "owner2"]
-    team        = "team"
-    environment = "dev"
+variable "calc_defaults" {
+  description  = "default values for the calc servers"
+  default      = {
+    chart      = "default chart name"
+    is_enabled = true
+    version    = "1.0.0"
   }
 }
 
 locals {
-#  consul_mi6_vars = {
-    #    "server.extraLabels"
-    #    "client.extraLabels"
-#  }
-
-  //    APP_NAME: AstraKafkaBroker0
-  //    BATCH: group
-  //    ENVIRONMENT: dev
-  //    Kubernetes_APP: group
-  //    NAMESPACE: astra
-  //    OWNER1: jrossi
-  //    OWNER2: mayhtim
-  //    OWNER3: apinkhas
-  //    Team: GroupDataOpsAstraDev
-#  consul_mi6_vars = merge(
-#  {for t in toset(["server", "client"]):
-#    "${t}.extraLabels" =>
-#      merge({
-#        Team     = var.mi6_labels.team,
-#        APP_NAME = "consul_${t}",
-#      },
-#      { for g in var.mi6_labels.groups:
-#        g => "group"
-#      }, { for i,o in var.mi6_labels.owners:
-#        "OWNER${i+1}" => o
-#      })
-#  })
-  # create an object that corresponds to:
-  #  APP_NAME: AstraKafkaBroker0
-  #  BATCH: group
-  #  ENVIRONMENT: dev
-  #  Kubernetes_APP: group
-  #  NAMESPACE: astra
-  #  OWNER1: jrossi
-  #  OWNER2: mayhtim
-  #  OWNER3: apinkhas
-  #  Team: GroupDataOpsAstraDev
-  consul_obj = merge({
-    Team        = var.mi6_labels.team,
-    ENVIRONMENT = var.mi6_labels.environment
-  },
-  { for g in var.mi6_labels.groups:
-    g => "group"
-  }, { for i,o in var.mi6_labels.owners:
-    "OWNER${i+1}" => o
-  })
-  # map to helm variables for client to create extra labels
-  consul_mi6_client_vars = merge({
-    for k,v in local.consul_obj:
-      "client.extraLabels.${k}" => v
-  }, {
-    "client.extraLabels.APP_NAME" = "ConsulClient"
-  })
-  # map to helm variables for server to create extra labels
-  consul_mi6_server_vars = merge({
-    for k,v in local.consul_obj:
-      "server.extraLabels.${k}" => v
-  }, {
-    "server.extraLabels.APP_NAME" = "ConsulServer"
-  })
-  // flatten out to single map
-  consul_mi6_vars = merge(local.consul_mi6_client_vars, local.consul_mi6_server_vars)
+  calc_variables = {
+    for k,v in var.calc_overrides:
+      # release link name
+      k => {
+        for dk, dv in var.calc_defaults:
+          # get the override value, if dne use the default one
+          dk => try(var.calc_overrides[k][dk], dv)
+      }
+  }
 }
 
-
 output "test" {
-  value = local.consul_mi6_vars
-#  value = local.consul_obj
+  value = local.calc_variables
 }
